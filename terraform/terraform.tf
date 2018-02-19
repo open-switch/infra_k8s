748e94c7b2335d16ad38ca333b68dead1b5cd043
Tyler Heucke <tyler.heucke@dell.com>
2018-02-19 09:49:57 -0800

    Store terraform state in S3
    Change-Id: I7bf05f476385a5fae0c4851c350fb83bc0b67e39
    Signed-off-by: Tyler Heucke <tyler.heucke@dell.com>

---
+++ terraform/terraform.tf
@@ -0,0 +1,39 @@
+# store terraform state in s3 with locking
+
+resource "aws_s3_bucket" "opx-terraform-state-store" {
+  bucket = "opx-terraform-state-store"
+  versioning {
+    enabled = true
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+  tags {
+    Name = "OPX Remote Terraform State Store"
+  }
+}
+
+resource "aws_dynamodb_table" "opx-terraform-state-lock" {
+  name = "opx-terraform-state-lock"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  tags {
+    Name = "OPX DynamoDB Terraform State Lock Table"
+  }
+}
+
+terraform {
+  backend "s3" {
+    encrypt = true
+    bucket = "opx-terraform-state-store"
+    dynamodb_table = "opx-terraform-state-lock"
+    region = "us-west-2"
+    key = "terraform.tfstate"
+  }
+}
+
